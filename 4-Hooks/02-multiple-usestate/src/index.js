e1e48dea8f423fa96939992439cba2dd3977b320
itsxdamdam <bakareayomiku@gmail.com>
2024-08-19 16:13:53 +0100

    multiple-usestate

--- 4-Hooks/02-multiple-usestate/src/index.js
+++ 4-Hooks/02-multiple-usestate/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import { createRoot} from "react-dom/client";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [water, setOunceWater] = useState(8);
+  const [coffee, setGramsCoffee] = useState(13);
+  return (
+    <div>
+      <h1>Coffe Maker</h1>
+      <span aria-hidden>☕</span>
+      <h2>Water</h2>
+      <button onClick={() => setOunceWater(water + 1)}>{water} oz</button>
+      <h2>Coffee</h2>
+      <button onClick={() => setGramsCoffee(coffee + 1)}>{coffee} g</button>
+    </div>
+  )
+}
+
+createRoot(
+  document.querySelector("#root")
+).render(
+  <App />
+)
