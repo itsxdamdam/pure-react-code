64d0fa38d5499387963b52dad05f0e55f38ebd9a
itsxdamdam <bakareayomiku@gmail.com>
2024-08-22 18:22:58 +0100

    loading-indicator

---
+++ 5-API-Requests/06-loading-indicator/src/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import Pet from './Pet';
+import './index.css';
+
+const App = () => {
+  const [pets, setPets] = useState([]);
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      try {
+        const res = await fetch("http://localhost:3001/pets");
+        const pets = await res.json();
+        setPets(pets);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false)
+      }
+      
+    }
+
+    getData();
+
+  // setLoading(true);
+  // fetch("http://localhost:3001/pets")
+  //   .then(res => res.json())
+  //   .then(pets => setPets(pets))
+  //   .finally(() => setLoading(false))
+  }, [])
+
+
+  return (
+    <main>
+      <h1>Adopt-a-Pet</h1>
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <>
+          <ul>
+            {pets.map(pet => (
+              <li key={pet.id}>
+                <Pet pet={pet} />
+              </li>
+            ))}
+          </ul>
+          <button>Add a Pet</button>
+        </>
+      )}
+    </main>
+  );
+};
+
+createRoot(document.querySelector("#root")).render(<App />)
