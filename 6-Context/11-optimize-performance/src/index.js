d90a54311f19b280c7289001a3f9bc87fe207f60
itsxdamdam <bakareayomiku@gmail.com>
2024-09-13 13:02:55 +0100

    optimize-performance

--- 6-Context/11-optimize-performance/src/index.js
+++ 6-Context/11-optimize-performance/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
+import { useUser } from './UserContext';
+import Providers from './Providers';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Root() {
+  const user = useUser();
+  return user ? <MainPage /> : <LoginPage />;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ 
+createRoot(document.querySelector("#root")).render(
+  <Providers>
+    <Root />
+  </Providers>
+)
