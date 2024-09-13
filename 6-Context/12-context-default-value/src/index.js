97213e520cd4cb62e1e633ecb6320eda9de5f77c
itsxdamdam <bakareayomiku@gmail.com>
2024-09-13 13:19:08 +0100

    context default value

--- 6-Context/12-context-default-value/src/index.js
+++ 6-Context/12-context-default-value/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useContext } from 'react';
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const Color = React.createContext();
+
+const FancyButton = props => {
+  const color = useContext(Color);
+
+  if(typeof color === "undefined") {
+    throw new Error ('FancyButton requires a Color Provider')
+  };
+
+  return (
+    <button className={`fancy-btn ${color}`}{...props}>
+      Click Me
+    </button>
+  )
+}
+
+const App = () => (
+  <div>
+    <FancyButton />
+    <Color.Provider value="red">
+      <FancyButton />
+    </Color.Provider>
+    <Color.Provider value="green">
+      <FancyButton />
+    </Color.Provider>
+    <Color.Provider value="blue">
+      <FancyButton />
+    </Color.Provider>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.querySelector('#root')).render(<App />)
